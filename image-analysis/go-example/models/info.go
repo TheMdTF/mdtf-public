8a0d391d126de7c919c62e1193f93f8a14a3d064
Carter Tilghman <john.c.tilghman@saic.com>
2019-11-15 14:21:41 -0500

    added image analysis yaml and models files

---
+++ image-analysis/go-example/models/info.go
@@ -0,0 +1,27 @@
+package models
+
+// Info Basic information describing the algorithm.
+type Info struct {
+
+	// Name of algorithm
+	AlgorithmName string
+
+	// A string enum describing the type of biometric images the algorithm is meant to process
+	// Enum: [Face Finger Iris]
+	AlgorithmType string
+
+	// Algorithm version identifier
+	AlgorithmVersion string
+
+	// Name of the Company which produces the algorithm
+	CompanyName string
+
+	// The recommended allocation of CPUs for the deployed docker container.
+	RecommendedCPUs int64
+
+	// The recommended allocation of memory (MB) for the deployed docker container.
+	RecommendedMem int64
+
+	// The email address of an engineer or other technical resource to contact in the event of an error running your service. This field may be left blank if desired.
+	TechnicalContactEmail string
+}
