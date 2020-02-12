6974edae8cf90895ae247f88504d12df63160b24
Carter Tilghman <57812184+jctilghman@users.noreply.github.com>
2020-02-12 16:47:33 -0500

    comment fix

--- image-analysis/go-example/models/image.go
+++ image-analysis/go-example/models/image.go
@@ -3,6 +3,6 @@ package models
 // Image Data transfer object for an image.
 type Image struct {
 
-	// The captured image data in PNG format, encoded as a base64 string. The data string shall not exceed 1MB.
+	// The captured image data in PNG format, encoded as a base64 string. The data string shall not exceed 10MB.
 	ImageData string
 }
